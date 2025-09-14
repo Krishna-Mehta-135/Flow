23b5a895dd21fac5769ea3e34f69626a6136a566
Krishna Mehta <krishnamehta135@protonmail.com>
2025-09-14 20:17:19 +0530

    Added basic backend and initilized the app

---
+++ Server/src/app.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+// CORS Configuration
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true, // ✅ required to allow cookies or Authorization headers
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+//Routes
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/content", contentRouter);
+// app.use("/api/v1/brain", brainRouter);
+
+export {app};
---
+++ Server/src/controllers/auth.controller.ts
@@ -0,0 +1,103 @@
+import jwt from "jsonwebtoken";
+import {User} from "../models/user.models";
+import {asyncHandler} from "../utils/asyncHandler";
+import mongoose, {ObjectId} from "mongoose";
+import {ApiResponse} from "../utils/ApiResponse";
+import {loginUserSchema, registerUserSchema} from "../validation/user.validation";
+import {Types} from "mongoose";
+
+const generateToken = (id: Types.ObjectId) => {
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in the environment variables");
+    }
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+    //User validation
+    const validationResult = registerUserSchema.safeParse(req.body);
+    if (!validationResult.success) {
+        const formatted = validationResult.error.format();
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: formatted,
+        });
+    }   
+
+    const {username, email, password} = validationResult.data;
+
+    //Check if user exists
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+        return res.status(403).json(new ApiResponse(403, null, "User already exists"));
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+    });
+
+    const token = generateToken(user._id);
+
+    return res.status(201).json(
+        new ApiResponse(
+            201,
+            {
+                user: {
+                    _id: user._id,
+                    username: user.username,
+                    email: user.email,
+                },
+                token,
+            },
+            "User registered successfully"
+        )
+    );
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    //Validate the result first
+    const validationResult = loginUserSchema.safeParse(req.body);
+    if (!validationResult.success) {
+        return res.status(400).json(new ApiResponse(400, null, "Enter correct credentials"));
+    }
+
+    //take the validated result
+    const {credential, password} = validationResult.data;
+
+    const user = await User.findOne({
+        $or: [{email: credential}, {username: credential}],
+    }).exec();
+
+    if (!user) {
+        return res.status(403).json(new ApiResponse(403, null, "Invalid Credentials"));
+    }
+
+    // Verify password using the comparePassword method from your User model
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+        return res.status(401).json(new ApiResponse(401, null, "Invalid credentials"));
+    }
+
+    const token = generateToken(user._id);
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {
+                user: {
+                    _id: user._id,
+                    username: user.username,
+                    email: user.email,
+                },
+                token,
+            },
+            "User logged in successfully"
+        )
+    );
+});
+
+export {registerUser, loginUser};
